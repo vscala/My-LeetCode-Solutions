a5f7f83d6bd5ea3f5a5a7fe15afc1b3a35a2ea2b
Vincent Scala <vincent2000nc@gmail.com>
2021-11-12 14:04:36 -0500

    Time: 5180 ms (20.81%), Space: 32.6 MB (87.36%) - LeetHub

--- count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -1,10 +1,8 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        right = []
-        out = []
-        nums.reverse()
-        for num in nums:
-            i = bisect_left(right, num)
+        right, out = [], []
+        for j in range(-1, ~len(nums), -1):
+            i = bisect_left(right, nums[j])
             out += [i]
-            right.insert(i, num)
+            right.insert(i, nums[j])
         return out[::-1]
