da18ca42a46e7ef25aa898a89a44c999047ed2b7
Vincent Scala <vincent2000nc@gmail.com>
2021-11-17 14:31:58 -0500

    Time: 36 ms (63.77%), Space: 14.4 MB (40.86%) - LeetHub

--- valid-square/valid-square.py
+++ valid-square/valid-square.py
@@ -1,18 +1,14 @@
 class Solution:
-    def sideLength(self, a, b):
-        return sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
-    
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
+        if p1 == p2 == p3 == p4: return False
         p1, p2, p3, p4 = sorted([p1,p2,p3,p4])
         
-        if p1 == p2 == p3 == p4: return False
+        sideLength = lambda a, b : (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
         
-        s1 = self.sideLength(p1, p2)
-        s2 = self.sideLength(p1, p3)
-        s3 = self.sideLength(p4, p2)
-        s4 = self.sideLength(p4, p3)
+        s1 = sideLength(p1, p2)
+        s2 = sideLength(p1, p3)
+        s3 = sideLength(p4, p2)
+        s4 = sideLength(p4, p3)
+        h  = sideLength(p2, p3)
         
-        if not (s1 == s2 == s3 == s4): return False
-        
-        h = self.sideLength(p2, p3)
-        return abs(h - sqrt(2)*s1) < .0001
+        return (s1 == s2 == s3 == s4) and h == 2 * s1
